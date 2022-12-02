187b791f66d1f44f8297e0bda4ac465de13248f4
Michael Byers <michaelbyers@Michaels-MacBook-Pro.local>
2022-12-01 19:01:38 -0500

    partial icon fix

--- main.js
+++ main.js
@@ -19,20 +19,26 @@ $(function(){
 
 // Function to convert Specific Gravity to Plato
 var convertSpecificGravityToPlato = function(specificGravityNumber) {
-  platoNumber = (-1 * 616.868) + (1111.14 * specificGravityNumber) - (630.272 * (specificGravityNumber * specificGravityNumber)) + (135.997 * (specificGravityNumber * specificGravityNumber * specificGravityNumber));
-  return platoNumber.toFixed(1);
+  if (specificGravityNumber > 0) {
+    platoNumber = (-1 * 616.868) + (1111.14 * specificGravityNumber) - (630.272 * (specificGravityNumber * specificGravityNumber)) + (135.997 * (specificGravityNumber * specificGravityNumber * specificGravityNumber));
+    return platoNumber.toFixed(1);
+  }
 };
 
 var calcSpecificGravityToPlato = document.querySelector('#button-gravity-to-plato');
 calcSpecificGravityToPlato.addEventListener('click', function(e) {
   var input = document.querySelector('#input-gravity-to-plato');
   input.value = convertSpecificGravityToPlato(input.value);
+
+  
 });
 
 // Function to convert Plato to Specific Gravity
 var convertPlatoToSpecificGravity = function(platoNumber) {
-  var specificGravityNumber = 1 + (platoNumber / (258.6 - ((platoNumber/258.2) * 227.1)));
-  return specificGravityNumber.toFixed(3);
+  if (platoNumber > 0) {
+    var specificGravityNumber = 1 + (platoNumber / (258.6 - ((platoNumber/258.2) * 227.1)));
+    return specificGravityNumber.toFixed(3);
+  }
 };
 
 var calcPlatoToSpecificGravity = document.querySelector('#button-plato-to-gravity');
@@ -43,8 +49,10 @@ calcPlatoToSpecificGravity.addEventListener('click', function(e) {
 
 // Function to convert sixteen ounce bottle cases to beer barrels
 var convertSixteenOunceCasesToBeerBarrels = function(sixteenOunceCases){
-  var beerBarrels = sixteenOunceCases * 0.09677;
-  return beerBarrels.toFixed(2);
+  if (sixteenOunceCases > 0) {
+    var beerBarrels = sixteenOunceCases * 0.09677;
+    return beerBarrels.toFixed(2);
+  }
 };
 
 var calcSixteenOunceCasesToBeerBarrels = document.querySelector('#button-sixteen-cases-to-beer-barrels');
@@ -55,8 +63,10 @@ calcSixteenOunceCasesToBeerBarrels.addEventListener('click', function(e) {
 
 // Function to convert twelve ounce bottle cases to beer barrels
 var convertTwelveOunceCasesToBeerBarrels = function(twelveOunceCases){
-  var beerBarrels = twelveOunceCases * 0.07258;
-  return beerBarrels.toFixed(2);
+  if (twelveOunceCases > 0) {
+    var beerBarrels = twelveOunceCases * 0.07258;
+    return beerBarrels.toFixed(2);
+  }
 };
 
 var calcTwelveOunceCasesToBeerBarrels = document.querySelector('#button-twelve-cases-to-beer-barrels');
@@ -67,8 +77,10 @@ calcTwelveOunceCasesToBeerBarrels.addEventListener('click', function(e) {
 
 // Function to convert sixth barrel kegs to beer barrels
 var convertSixthBarrelKegsToBeerBarrels = function(sixthBarrelKegs){
-  var beerBarrels = sixthBarrelKegs * 0.1667742;
-  return beerBarrels.toFixed(2);
+  if (sixthBarrelKegs > 0) {
+    var beerBarrels = sixthBarrelKegs * 0.1667742;
+    return beerBarrels.toFixed(2);
+  }
 };
 
 var calcSixthBarrelKegsToBeerBarrels = document.querySelector('#button-sixth-barrel-kegs-to-beer-barrels');
@@ -79,8 +91,10 @@ calcSixthBarrelKegsToBeerBarrels.addEventListener('click', function(e) {
 
 // Function to convert half barrel kegs to beer barrels
 var convertHalfBarrelKegsToBeerBarrels = function(halfBarrelKegs){
-  var beerBarrels = halfBarrelKegs * 0.5;
-  return beerBarrels.toFixed(2);
+  if (halfBarrelKegs > 0) {
+    var beerBarrels = halfBarrelKegs * 0.5;
+    return beerBarrels.toFixed(2);
+  }
 };
 
 var calcHalfBarrelKegsToBeerBarrels = document.querySelector('#button-half-barrel-kegs-to-beer-barrels');
@@ -91,8 +105,10 @@ calcHalfBarrelKegsToBeerBarrels.addEventListener('click', function(e) {
 
 // Function to convert 20L key kegs to beer barrels
 var convertTwentyLiterLKeyKegsToBeerBarrels = function(twentyLiterKeyKegs){
-  var beerBarrels = twentyLiterKeyKegs * 0.171;
-  return beerBarrels.toFixed(2);
+  if (twentyLiterKeyKegs > 0) {
+    var beerBarrels = twentyLiterKeyKegs * 0.171;
+    return beerBarrels.toFixed(2);
+  }
 };
 
 var calcTwentyLiterKeyKegsToBeerBarrels = document.querySelector('#button-twenty-liter-key-kegs-to-beer-barrels');
@@ -102,49 +118,57 @@ calcTwentyLiterKeyKegsToBeerBarrels.addEventListener('click', function(e) {
 });
 
 // Function to convert 375mL bottles to beer barrels
-var convert375mLBottlesToBeerBarrels = function(_375mLBottles){
-  var beerBarrels = _375mLBottles * 0.038407;
-  return beerBarrels.toFixed(2);
+var convert375mLBottleCasesToBeerBarrels = function(_375mLBottleCases){
+  if (_375mLBottleCases > 0) {
+    var beerBarrels = _375mLBottleCases * 0.038407;
+    return beerBarrels.toFixed(2);
+  }
 };
 
-var calc375mLBottlesToBeerBarrels = document.querySelector('#button-375mL-bottles-to-beer-barrels');
-calc375mLBottlesToBeerBarrels.addEventListener('click', function(e) {
+var calc375mLBottleCasesToBeerBarrels = document.querySelector('#button-375mL-bottles-to-beer-barrels');
+calc375mLBottleCasesToBeerBarrels.addEventListener('click', function(e) {
   var input = document.querySelector('#input-375mL-bottles-to-beer-barrels');
-  input.value = convert375mLBottlesToBeerBarrels(input.value);
+  input.value = convert375mLBottleCasesToBeerBarrels(input.value);
 });
 
 // Function to convert 500mL bottles to beer barrels
-var convert500mLBottlesToBeerBarrels = function(_500mLBottles){
-  var beerBarrels = _500mLBottles * 0.051109;
-  return beerBarrels.toFixed(2);
+var convert500mLBottleCasesToBeerBarrels = function(_500mLBottleCases){
+  if (_500mLBottleCases > 0) {
+    var beerBarrels = _500mLBottleCases * 0.051109;
+    return beerBarrels.toFixed(2);
+  }
 };
 
-var calc500mLBottlesToBeerBarrels = document.querySelector('#button-500mL-bottles-to-beer-barrels');
-calc500mLBottlesToBeerBarrels.addEventListener('click', function(e) {
+var calc500mLBottleCasesToBeerBarrels = document.querySelector('#button-500mL-bottles-to-beer-barrels');
+calc500mLBottleCasesToBeerBarrels.addEventListener('click', function(e) {
   var input = document.querySelector('#input-500mL-bottles-to-beer-barrels');
-  input.value = convert500mLBottlesToBeerBarrels(input.value);
+  input.value = convert500mLBottleCasesToBeerBarrels(input.value);
 });
 
 // Function to convert 22oz bottles to beer barrels
-var convert22ozBottlesToBeerBarrels = function(_22ozBottles){
-  var beerBarrels = _22ozBottles * 0.06653;
-  return beerBarrels.toFixed(2);
+var convert22ozBottleCasesToBeerBarrels = function(_22ozBottleCases){
+  if (_22ozBottleCases > 0) {
+    var beerBarrels = _22ozBottleCases * 0.06653;
+    return beerBarrels.toFixed(2);
+  }
 };
 
-var calc22ozBottlesToBeerBarrels = document.querySelector('#button-22oz-bottles-to-beer-barrels');
-calc22ozBottlesToBeerBarrels.addEventListener('click', function(e) {
+var calc22ozBottleCasesToBeerBarrels = document.querySelector('#button-22oz-bottles-to-beer-barrels');
+calc22ozBottleCasesToBeerBarrels.addEventListener('click', function(e) {
   var input = document.querySelector('#input-22oz-bottles-to-beer-barrels');
-  input.value = convert22ozBottlesToBeerBarrels(input.value);
+  input.value = convert22ozBottleCasesToBeerBarrels(input.value);
 });
 
-// Function to convert 750mL bottles to beer barrels
-var convert750mLBottlesToBeerBarrels = function(_750mLBottles){
-  var beerBarrels = _750mLBottles * 0.076814516;
-  return beerBarrels.toFixed(2);
+// Function to convert 750mL bottle cases to beer barrels
+var convert750mLBottleCasesToBeerBarrels = function(_750mLBottleCases){
+  if (_750mLBottleCases > 0) {
+    var beerBarrels = _750mLBottleCases * 0.076814516;
+    return beerBarrels.toFixed(2);
+  }
 };
 
-var calc750mLBottlesToBeerBarrels = document.querySelector('#button-750mL-bottles-to-beer-barrels');
-calc750mLBottlesToBeerBarrels.addEventListener('click', function(e) {
+var calc750mLBottleCasesToBeerBarrels = document.querySelector('#button-750mL-bottles-to-beer-barrels');
+calc750mLBottleCasesToBeerBarrels.addEventListener('click', function(e) {
   var input = document.querySelector('#input-750mL-bottles-to-beer-barrels');
-  input.value = convert750mLBottlesToBeerBarrels(input.value);
+  input.value = convert750mLBottleCasesToBeerBarrels(input.value);
 });
